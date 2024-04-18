de4286e1eb105becb51e26b000efaf221002a900
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-18 20:28:04 -0300

    atividade nome completo

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+let name = prompt ("Digite seu primerio nome");
+let sobrenome = prompt ("Digite seu sobrenome");
+
+
+alert ("Nome completo:  " + name + '  ' + sobrenome) ;
+
+alert ("Nome de catálogo:  "  + sobrenome.toLocaleUpperCase() + " , " + name);
+
+
+
